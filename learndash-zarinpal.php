81af1b03618a7aadbd3ed39aa7f9a546ff271700
Amir Hossein Taghizadeh <amyroseintz@gmail.com>
2024-03-13 02:29:41 +0330

    Update learndash-zarinpal.php

--- learndash-zarinpal.php
+++ learndash-zarinpal.php
@@ -5,7 +5,7 @@
  * Description: درگاه پرداخت زرین پال برای افزونه لرن دش LearnDash
  * Version: 2.0.1
  * Author: Amirhossein Taghizadeh
- * Author URI: http://www.Amyrosein.ir/
+ * Author URI: http://www.AmirTz.ir/
  * Text Domain: leandash-for-zarinpal
  * Domain Path: languages
  */
